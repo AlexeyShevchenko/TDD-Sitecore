e39e1135e8bb82d77df1d740bd18f55075f33cfa
Alexey Shevchenko <alekseiwe@gmail.com>
2017-09-21 12:19:33 +0300

    initial commit implement DoSomething command write tests which cover QueryState method of DoSomething

---
+++ CustomCommand/DoSomething.cs
@@ -0,0 +1,23 @@
+﻿namespace CustomCommand
+{
+    using Sitecore.Data;
+    using Sitecore.Shell.Framework.Commands;
+
+    public class DoSomething : Command
+    {
+        public override void Execute(CommandContext context) { }
+
+        public override CommandState QueryState(CommandContext context)
+        {
+            var folderId = new ID("{B4F0DAD0-B60E-49D9-8332-8DF1A61C5101}");
+            var folder = Sitecore.Context.Database.GetItem(folderId);
+
+            if (folder == null)
+            {
+                return CommandState.Hidden;
+            }
+
+            return folder.HasChildren ? CommandState.Enabled : CommandState.Hidden;
+        }
+    }
+}
---
+++ CustomCommandTests/DoSomethingTests.cs
@@ -0,0 +1,75 @@
+﻿namespace CustomCommandTests
+{
+    using CustomCommand;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using Sitecore.Data;
+    using Sitecore.FakeDb;
+    using Sitecore.Shell.Framework.Commands;
+
+    public class DoSomethingTests
+    {
+        [TestCase]
+        public void CommandState_ShouldBeHidden_WhenFolderDoesNotExist()
+        {
+            // arrange
+            var doSomethingCommand = new DoSomething();
+            var commandContext = new CommandContext();
+
+            using (var db = new Db { })
+            {
+                // act
+                var commandState = doSomethingCommand.QueryState(commandContext);
+
+                // assert
+                commandState.Should().Be(CommandState.Hidden);
+            }
+        }
+
+        [TestCase]
+        public void CommandState_ShouldBeHidden_WhenNoChildren()
+        {
+            // arrange
+            var folderId = new ID("{B4F0DAD0-B60E-49D9-8332-8DF1A61C5101}");
+            var doSomethingCommand = new DoSomething();
+            var commandContext = new CommandContext();
+
+            using (var db = new Db
+            {
+                new DbItem("Folder", folderId)
+            })
+            {
+                // act
+                var commandState = doSomethingCommand.QueryState(commandContext);
+
+                // assert
+                commandState.Should().Be(CommandState.Hidden);
+            }
+        }
+
+        [TestCase]
+        public void CommandState_ShouldBeHidden_WhenChildrenExist()
+        {
+            // arrange
+            var folderId = new ID("{B4F0DAD0-B60E-49D9-8332-8DF1A61C5101}");
+            var doSomethingCommand = new DoSomething();
+            var commandContext = new CommandContext();
+
+            using (var db = new Db
+            {
+                new DbItem("Folder", folderId)
+                {
+                    new DbItem("child 1"),
+                    new DbItem("child 2")
+                }
+            })
+            {
+                // act
+                var commandState = doSomethingCommand.QueryState(commandContext);
+
+                // assert
+                commandState.Should().Be(CommandState.Enabled);
+            }
+        }
+    }
+}
