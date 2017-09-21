d046e5aa2f756468561332fd80c0f141daab37da
Alexey Shevchenko <alekseiwe@gmail.com>
2017-09-21 15:51:02 +0300

    create scenario when command button should be enabled when folder contains any items with 'Start Date' value larger than DateTime.Now

---
+++ CustomCommand/Example2.cs
@@ -0,0 +1,37 @@
+﻿namespace CustomCommand
+{
+    using Sitecore;
+    using Sitecore.Data;
+    using Sitecore.Shell.Framework.Commands;
+    using System;
+    using System.Linq;
+
+    public class Example2 : Command
+    {
+        public override void Execute(CommandContext context) { }
+
+        public override CommandState QueryState(CommandContext context)
+        {
+            var folderId = new ID("{B4F0DAD0-B60E-49D9-8332-8DF1A61C5101}");
+            var folder = Context.Database.GetItem(folderId);
+
+            if (folder == null)
+            {
+                return CommandState.Hidden;
+            }
+
+            if (folder.HasChildren
+                && folder.Children.Any(c => DateUtil.IsoDateToDateTime(c["Start Date"]) > GetDateTimeNow()))
+            {
+                return CommandState.Enabled;
+            }
+
+            return CommandState.Hidden;
+        }
+
+        public virtual DateTime GetDateTimeNow()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ CustomCommandTests/Example2Tests.cs
@@ -0,0 +1,77 @@
+﻿namespace CustomCommandTests
+{
+    using System;
+    using CustomCommand;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using Sitecore.Data;
+    using Sitecore.FakeDb;
+    using Sitecore.Shell.Framework.Commands;
+
+    public class Example2Tests
+    {
+        [TestCase]
+        public void CommandState_ShouldBeHidden_WhenNoOneItemsContainStartDateLargerDateTimeNow()
+        {
+            // arrange
+            var folderId = new ID("{B4F0DAD0-B60E-49D9-8332-8DF1A61C5101}");
+            var doSomethingCommand = new FakeExample2 { FakeDateTimeNow = new DateTime(2018, 1, 1, 0, 0, 0) };
+            var commandContext = new CommandContext();
+
+            using (var db = new Db
+            {
+                new DbItem("Folder", folderId)
+                {
+                    new DbItem("child 1")
+                    {
+                        new DbField("Start Date") { Value = "20170000T000000" }
+                    }
+                }
+            })
+            {
+                // act
+                var commandState = doSomethingCommand.QueryState(commandContext);
+
+                // assert
+                commandState.Should().Be(CommandState.Hidden);
+            }
+        }
+
+        [TestCase]
+        public void CommandState_ShouldBeHidden_WhenExistItemContainsStartDateLargerDateTimeNow()
+        {
+            // arrange
+            var folderId = new ID("{B4F0DAD0-B60E-49D9-8332-8DF1A61C5101}");
+            var doSomethingCommand = new FakeExample2 { FakeDateTimeNow = new DateTime(2018, 1, 1, 0, 0, 0) };
+            var commandContext = new CommandContext();
+
+            using (var db = new Db
+            {
+                new DbItem("Folder", folderId)
+                {
+                    new DbItem("child 1")
+                    {
+                        new DbField("Start Date") { Value = "20181231T235959" }
+                    }
+                }
+            })
+            {
+                // act
+                var commandState = doSomethingCommand.QueryState(commandContext);
+
+                // assert
+                commandState.Should().Be(CommandState.Enabled);
+            }
+        }
+    }
+
+    public class FakeExample2 : Example2
+    {
+        public DateTime FakeDateTimeNow { get; set; }
+
+        public override DateTime GetDateTimeNow()
+        {
+            return FakeDateTimeNow;
+        }
+    }
+}
