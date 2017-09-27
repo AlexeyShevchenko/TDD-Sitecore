1cf19bc118e0b39b7895a1c9e54c4c9f0a3c213d
Alexey Shevchenko <alekseiwe@gmail.com>
2017-09-27 11:41:36 +0300

    create test of protected method

---
+++ CustomCommand/CustomField.cs
@@ -0,0 +1,26 @@
+﻿namespace CustomCommand
+{
+    using Sitecore.Diagnostics;
+    using Sitecore.Shell.Applications.ContentEditor;
+    using System.Web.UI;
+
+    public class CustomField : Sitecore.Web.UI.HtmlControls.Control, IContentField
+    {
+        public string GetValue()
+        {
+            return this.Value;
+        }
+
+        public void SetValue(string value)
+        {
+            this.Value = value;
+        }
+
+        protected override void DoRender(HtmlTextWriter output)
+        {
+            Assert.ArgumentNotNull(output, "output");
+
+            output.Write("Hello from CustomField");
+        }
+    }
+}
---
+++ CustomCommandTests/CustomFieldTests.cs
@@ -0,0 +1,36 @@
+﻿namespace CustomCommandTests
+{
+    using CustomCommand;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using System.IO;
+    using System.Web.UI;
+
+    public class CustomFieldTests
+    {
+        [TestCase]
+        public void DoRender_ShouldFillOutput()
+        {
+            // arrange
+            var customField = new FakeCustomField();
+            var output = new HtmlTextWriter(new StringWriter());
+
+            // assert
+            output.InnerWriter.ToString().Should().BeEmpty();
+
+            // act
+            customField.FakeDoRender(output);
+
+            // assert
+            output.InnerWriter.ToString().Should().Be("Hello from CustomField");
+        }
+
+        class FakeCustomField : CustomField
+        {
+            public void FakeDoRender(HtmlTextWriter output)
+            {
+                base.DoRender(output);
+            }
+        }
+    }
+}
