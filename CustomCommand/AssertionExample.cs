ef9b35cb624c28c3266ab5fcc598228304d9d3a2
Alexey Shevchenko <alekseiwe@gmail.com>
2017-09-28 11:48:09 +0300

    assertion tests

---
+++ CustomCommand/AssertionExample.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomCommand
+{
+    using Sitecore.Diagnostics;
+
+    public class AssertionExample
+    {
+        public void DoSomething(SomeData someData)
+        {
+            Assert.ArgumentNotNull(someData, "someData");
+            Assert.ArgumentNotNullOrEmpty(someData.SomeString, "someData.SomeString");
+
+            // TODO
+        }
+    }
+
+    public class SomeData
+    {
+        public string SomeString { get; set; }
+    }
+}
---
+++ CustomCommandTests/AssertionExampleTests.cs
@@ -0,0 +1,42 @@
+﻿namespace CustomCommandTests
+{
+    using CustomCommand;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using System;
+
+    public class AssertionExampleTests
+    {
+        [TestCase]
+        public void DoSomething_ShouldThrowArgumentNullException_WhenSomeDataIsNull()
+        {
+            // arrange
+            var assertionExample = new AssertionExample();
+
+            // act
+            Action action = () => assertionExample.DoSomething(null);
+
+            // assert
+            action.ShouldThrowExactly<ArgumentNullException>();
+        }
+
+        [TestCase]
+        public void DoSomething_ShouldThrowArgumentException_WhenSomeStringIsEmpty()
+        {
+            // arrange
+            var assertionExample = new AssertionExample();
+            var someData = new SomeData { SomeString = string.Empty };
+
+            // act
+            Action action = () => assertionExample.DoSomething(someData);
+
+            // assert
+            action.ShouldThrowExactly<ArgumentException>();
+
+            // act
+            // assert
+            try { action(); }
+            catch (ArgumentException ex) { ex.Message.Should().Contain("someData.SomeString"); }
+        }
+    }
+}
